baf873355b60bfc06a8b79b8ad7b1663b70e5c6b
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-03-08 11:19:15 +0100

    threaded permute

--- src/expression.jl
+++ src/expression.jl
@@ -297,7 +297,13 @@ end
 
 export permute_all_sum_indices
 function permute_all_sum_indices(ex::Expression)
-    Expression([permute_all_sum_indices(t) for t in ex.terms])
+    terms = copy(ex.terms)
+
+    Threads.@threads for i in eachindex(terms)
+        terms[i] = permute_all_sum_indices(terms[i])
+    end
+
+    Expression(terms)
 end
 
 # Commutator:
