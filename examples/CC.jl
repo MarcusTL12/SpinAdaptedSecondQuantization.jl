bb1762f8de31d8fbd2f347558372d22131d30545
Tor S. Haugland <tor.s.haugland@ntnu.no>
2023-03-28 14:39:26 -0400

    Clean

--- examples/CC.jl
+++ examples/CC.jl
@@ -42,15 +42,15 @@ end
 Tn(n) = 1 // factorial(n) * ∑(psym_tensor("t", 1:2n...) * τ(n), 1:2n)
 
 # Define Hamiltonian in terms of F and g
-Φ = 1 // 2 * ∑(psym_tensor("g", 1,2,3,4) * e(1,2,3,4), 1:4) +
-    ∑(-2 * psym_tensor("g", 1, 2, 3, 3) * E(1,2) * occupied(3), 1:3) +
-    ∑(psym_tensor("g", 1, 3, 3, 2) * E(1,2)* occupied(3) , 1:3)
+Φ = 1//2 * ∑(psym_tensor("g", 1,2,3,4) * e(1,2,3,4), 1:4) +
+    ∑(-2 * psym_tensor("g", 1,2,3,3) * E(1,2) * occupied(3), 1:3) +
+    ∑( 1 * psym_tensor("g", 1,3,3,2) * E(1,2) * occupied(3), 1:3)
 
 F = ∑(real_tensor("F", 1, 2) * E(1, 2), 1:2)
 
 H = F + Φ
 
-# Solve CCSDT equations (~2 min)
+# Solve CCSDT equations (~2 min / 8 threads)
 # Note: external symmetries ai<->bj<->ck is not found
 T = Tn(2) + Tn(3)
 Ecorr = cc_ket(H, T, 1, 0)
