c9bd736b41c67f04e2b3a82b7410cdf40e2b85bf
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-01-30 09:13:21 -0500

    Generate new format

--- src/code_generators/eT_code.jl
+++ src/code_generators/eT_code.jl
@@ -29,39 +29,10 @@ function print_eT_function_generator(name, ex::Expression, symbol, indices,
         GeneralOrbital => "g",
     ])
 
-    function make_param_def(julianame, name, spaces, flag)
+    function make_param_def(julianame, name)
+        is_inp = julianame ∉ noinput_tensors
         io = IOBuffer()
-        if isempty(spaces)
-            funcname = if flag == :I && julianame ∉ noinput_tensors
-                "input_scalar"
-            elseif flag == :O
-                "output_scalar"
-            else
-                "Sym"
-            end
-            print(io, "$julianame = $funcname(\"$name\")")
-        else
-            funcname = if flag == :I && julianame ∉ noinput_tensors
-                "input_tensor"
-            elseif flag == :O
-                "output_tensor"
-            else
-                "noio_tensor"
-            end
-            print(io, "$julianame = $funcname(\"$name\", (")
-            isfirst = true
-            for space in spaces
-                if isfirst
-                    isfirst = false
-                else
-                    print(io, ", ")
-                end
-                print(io, '"', space_dim_dict[space], '"')
-            end
-
-            print(io, "))")
-        end
-
+        print(io, "$julianame = (\"$name\", $is_inp)")
         String(take!(io))
     end
 
@@ -69,7 +40,11 @@ function print_eT_function_generator(name, ex::Expression, symbol, indices,
 
     outspaces = [translation(p)[1] for p in indices]
 
+    outdims = [getshortname(s) for s in outspaces]
+
     func_body = IOBuffer()
+
+    println(func_body, "func = FortranFunction((\"$symbol\", $outdims))")
 
     index_names = String[]
     io = IOBuffer()
@@ -77,8 +52,6 @@ function print_eT_function_generator(name, ex::Expression, symbol, indices,
         print_mo_index(io, ex[1].constraints, translation, p)
         push!(index_names, String(take!(io)))
     end
-
-    all_index_names = Dict()
 
     tensor_body = IOBuffer()
 
@@ -92,55 +65,19 @@ function print_eT_function_generator(name, ex::Expression, symbol, indices,
 
         local_trans = update_index_translation(t, translation)
 
-        term_indices = get_all_indices(t)
+        print(tensor_body, "update_code!(func, ein\"")
 
-        for p in term_indices
-            io = IOBuffer()
-            print_mo_index(io, t.constraints, local_trans, p)
-            n = String(take!(io))
-            if !haskey(all_index_names, n)
-                s = t.constraints(p)
-                fac = if s <: OccupiedOrbital
-                    1
-                elseif s <: VirtualOrbital
-                    10
-                else
-                    11
-                end
+        tensor_list = IOBuffer()
 
-                all_index_names[n] = fac
-            end
-        end
-
-        print(tensor_body, "    $symbol")
-        isfirst = true
-        print(tensor_body, "[")
-        for x in index_names
-            if isfirst
-                isfirst = false
-            else
-                print(tensor_body, ",")
-            end
-            print(tensor_body, "$x")
-        end
-        print(tensor_body, "]")
-
-        print(tensor_body, " += ")
+        print(tensor_list, "[")
 
         isfirst2 = true
-
-        if isone(-t.scalar)
-            print(tensor_body, "-")
-        elseif !isone(t.scalar)
-            printscalar(tensor_body, t.scalar)
-            isfirst2 = false
-        end
-
         for tens in t.tensors
             if isfirst2
                 isfirst2 = false
             else
-                print(tensor_body, "*")
+                print(tensor_body, ",")
+                print(tensor_list, ", ")
             end
 
             inds = get_indices(tens)
@@ -151,62 +88,38 @@ function print_eT_function_generator(name, ex::Expression, symbol, indices,
             tens_name = get_tensor_name(block_name, length(inds))
             julia_name = get_block_name(get_symbol(tens), spaces)
 
-            param_def = make_param_def(julia_name, tens_name, spaces, :I)
+            param_def = make_param_def(julia_name, tens_name)
 
             if param_def ∉ parameters
                 push!(parameters, param_def)
             end
 
-            print(tensor_body, julia_name)
+            print(tensor_list, julia_name)
 
-            isfirstinner = true
             for p in inds
-                if isfirstinner
-                    print(tensor_body, "[")
-                    isfirstinner = false
-                else
-                    print(tensor_body, ",")
-                end
                 print_mo_index(tensor_body, t.constraints, local_trans, p)
-            end
-            if !isfirstinner
-                print(tensor_body, "]")
             end
         end
 
-        println(tensor_body, "")
+        print(tensor_body, "->")
+
+        for p in index_names
+            print(tensor_body, p)
+        end
+
+        print(tensor_list, "]")
+
+        println(tensor_body, "\", ", t.scalar,
+            ", ", String(take!(tensor_list)), ")")
     end
-
-    println(func_body, "reset_state()")
-
-    println(func_body, make_param_def(symbol, symbol, outspaces, :O))
 
     for param in parameters
         println(func_body, param)
     end
 
-    all_index_names = sort!(collect(all_index_names))
-
-    print(func_body, "@tensor opt=(")
-    isfirst = true
-    for (n, f) in all_index_names
-        if isfirst
-            isfirst = false
-        else
-            print(func_body, ",")
-        end
-        print(func_body, "$n=>")
-        if f != 1
-            print(func_body, f)
-        end
-        print(func_body, "χ")
-    end
-    println(func_body, ") backend=eTbackend begin")
-
     print(func_body, String(take!(tensor_body)))
-    println(func_body, "end")
 
-    print(func_body, "finalize_eT_function(\"$name\", \"$wf_type\")")
+    print(func_body, "finalize_eT_function(func, \"$name\", \"$wf_type\")")
 
     io = IOBuffer()
 
