3c445f62ad388ba20fa6f6f211759bfaccc620e6
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-07-24 16:56:19 +0200

    Added doctstrings to default index constraints

--- src/index_space_definitions.jl
+++ src/index_space_definitions.jl
@@ -10,14 +10,68 @@ add_space_sum(OccupiedOrbital, VirtualOrbital, GeneralOrbital)
 
 export occupied, virtual, electron
 
+"""
+    occupied(indices...)
+
+Constraining the given indices to the OccupiedOrbital index space.
+
+# Example
+
+```jldoctest
+julia> using SpinAdaptedSecondQuantization
+
+julia> occupied(1, 2)
+C(i∈o, j∈o)
+
+julia> E(1, 2) * ans
+E_ij
+
+```
+"""
 function occupied(indices...)
     constrain(p => OccupiedOrbital for p in indices)
 end
 
+"""
+    virtual(indices...)
+
+Constraining the given indices to the VirtualOrbital index space.
+
+# Example
+
+```jldoctest
+julia> using SpinAdaptedSecondQuantization
+
+julia> virtual(1, 2)
+C(a∈v, b∈v)
+
+julia> E(1, 2) * ans
+E_ab
+
+```
+"""
 function virtual(indices...)
     constrain(p => VirtualOrbital for p in indices)
 end
 
+"""
+    electron(indices...)
+
+Constraining the given indices to the GeneralOrbital index space.
+
+# Example
+
+```jldoctest
+julia> using SpinAdaptedSecondQuantization
+
+julia> electron(1, 2)
+C(p∈g, q∈g)
+
+julia> E(1, 2) * ans
+E_pq
+
+```
+"""
 function electron(indices...)
     constrain(p => GeneralOrbital for p in indices)
 end
